88fdd9856e658e63902972e78ad0d70a9bae27b5
Vasyl Cherkes <cherkesva@gmail.com>
2025-09-23 00:18:49 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,13 @@
-# write your code here
+def copy_file(command: str) -> None:
+    try:
+        com, file_from, new_file = command.split()
+        if com == "cp" and file_from != new_file:
+            with (
+                open(file_from, "r") as file_in,
+                open(new_file, "w") as file_out
+            ):
+                file_out.write(file_in.read())
+        else:
+            pass
+    except Exception:
+        pass
